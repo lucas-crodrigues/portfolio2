33f699e0d8887a0117836793cef9566e1427a99f
lucascr <lcrodrigues1995@gmail.com>
2023-01-23 09:56:08 -0300

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const about = () => {
+const About = () => {
   return (
-    <div>about</div>
+    <div>About</div>
   )
 }
 
-export default about
+export default About
