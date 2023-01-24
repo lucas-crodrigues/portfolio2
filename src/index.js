38e286e99f54dee63378c6b77c1a22d82da445a3
lucascr <lcrodrigues1995@gmail.com>
2023-01-24 15:56:30 -0300

    Render using createRoot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css';
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <>
+  <App />
+  </>
+)
