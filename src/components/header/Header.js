2ac92cc7782e1c96bbe6de5d3d0cb33e9d18cfd2
lucascr <lcrodrigues1995@gmail.com>
2023-02-06 11:28:07 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+      <header>
+        <div className='container header-container'>
+          <h5>Hello I'm</h5>
+          <h1>Lucas Rodrigues</h1>
+          <h5 className='text-light'>Fullstack Developer</h5>
+          <CTA />
+        </div>
+      </header>
+    )
 }
 
 export default Header
