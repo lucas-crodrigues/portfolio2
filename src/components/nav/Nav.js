31bdd25ac008dd2680bf704283c3b31515e28e0a
lucascr <lcrodrigues1995@gmail.com>
2023-02-07 15:33:51 -0300

    Update Nav.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineAlignLeft} from 'react-icons/ai'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+import {AiOutlineTrophy} from 'react-icons/ai'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='nav-bar'>
+      <a href='#'><AiOutlineHome/> Home</a>
+      <a href='#about'><AiOutlineAlignLeft/> About</a>
+      <a href='#services'><AiOutlineFundProjectionScreen/> Services</a>
+      <a href='#portfolio'><AiOutlineFolderOpen/> Portfolio</a>
+      <a href='#testimonials'><AiOutlineTrophy/> Testimonials</a>
+      <a href='#contact'><AiOutlineMail/> Contact</a>
+    </nav>
   )
 }
 
