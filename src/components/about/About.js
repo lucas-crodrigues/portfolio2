882b33cdf3cae7fc1e8e4fe30875e259dda82ad7
lucascr <lcrodrigues1995@gmail.com>
2023-02-07 16:13:03 -0300

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './about.css'
+import About_me from './about_me'
+import lucas_about from '../../assets/lucas_about.jpg'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <div className='container about-container'>
+        <div className='about-picture'>
+          <img src={lucas_about} alt='Another picture of me' />
+        </div>
+        <About_me />
+      </div>
+    </section>
   )
 }
 
