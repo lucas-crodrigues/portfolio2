120ad98dc98bc6986c101118919014e2611ca002
lucascr <lcrodrigues1995@gmail.com>
2023-02-07 15:33:21 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import './header.css'
 import CTA from './CTA'
+import me from '../../assets/lucas.png'
 
 const Header = () => {
   return (
       <section id='home'>
         <div className='container header-container'>
-          <h5>Hello I'm</h5>
-          <h1>Lucas Rodrigues</h1>
-          <h5 className='text-light'>Fullstack Developer</h5>
-          <CTA />
+          <div>
+            <h5>Hello I'm</h5>
+            <h1>Lucas Rodrigues</h1>
+            <h5 className='text-light'>Fullstack Developer</h5>
+            <CTA />
+          </div>
+          <div className='header-photo'>
+            <img src={me} alt='Picture of myself'/>
+          </div>
         </div>
       </section>
     )
