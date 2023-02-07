fb3b390de69338a44719a5eb33b9546e9c0b5a1a
lucascr <lcrodrigues1995@gmail.com>
2023-02-07 12:17:14 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,14 @@ import CTA from './CTA'
 
 const Header = () => {
   return (
-      <header>
+      <section id='home'>
         <div className='container header-container'>
           <h5>Hello I'm</h5>
           <h1>Lucas Rodrigues</h1>
           <h5 className='text-light'>Fullstack Developer</h5>
           <CTA />
         </div>
-      </header>
+      </section>
     )
 }
 
