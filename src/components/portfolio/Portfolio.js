93335a9e4fd431b634b985c08f8406605900f6b4
lucascr <lcrodrigues1995@gmail.com>
2023-02-08 18:12:00 -0300

    Update Portfolio.js

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -11,8 +11,8 @@ import { AiOutlineGithub, AiOutlineGlobal } from 'react-icons/ai'
 const Portfolio = () => {
   return (
     <section id='portfolio'>
+      <h2>My recent work</h2>
       <div className='container portfolio-container'>
-        <h2>My recent work</h2>
         <div className='projects'>
           <article className='project'>
             <div className='project-image'>
@@ -20,8 +20,8 @@ const Portfolio = () => {
             </div>
             <h4>Bike Rentals</h4>
             <div className='project-actions'>
-              <a href='https://github.com/AbrahaKahsay/final-capstone-frontend' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://ducati-rentals.netlify.app/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/AbrahaKahsay/final-capstone-frontend' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://ducati-rentals.netlify.app/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
           <article className='project'>
@@ -30,8 +30,8 @@ const Portfolio = () => {
             </div>
             <h4>Free Games Database</h4>
             <div className='project-actions'>
-              <a href='https://github.com/lucas-crodrigues/free-games-db' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://free-games-db.netlify.app/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/lucas-crodrigues/free-games-db' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://free-games-db.netlify.app/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
           <article className='project'>
@@ -40,8 +40,8 @@ const Portfolio = () => {
             </div>
             <h4>Expenser App</h4>
             <div className='project-actions'>
-              <a href='https://github.com/lucas-crodrigues/rails-capstone' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://expenser.onrender.com/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/lucas-crodrigues/rails-capstone' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://expenser.onrender.com/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
           <article className='project'>
@@ -50,8 +50,8 @@ const Portfolio = () => {
             </div>
             <h4>Magic Cards Collection</h4>
             <div className='project-actions'>
-              <a href='https://github.com/lucas-crodrigues/js-capstone' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://lucas-crodrigues.github.io/js-capstone/dist/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/lucas-crodrigues/js-capstone' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://lucas-crodrigues.github.io/js-capstone/dist/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
           <article className='project'>
@@ -60,8 +60,8 @@ const Portfolio = () => {
             </div>
             <h4>Bookstore</h4>
             <div className='project-actions'>
-              <a href='https://github.com/lucas-crodrigues/bookstore' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://bookstore-lucas.netlify.app/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/lucas-crodrigues/bookstore' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://bookstore-lucas.netlify.app/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
           <article className='project'>
@@ -70,8 +70,8 @@ const Portfolio = () => {
             </div>
             <h4>To-Do List</h4>
             <div className='project-actions'>
-              <a href='https://github.com/lucas-crodrigues/to-do-list' className='btn' target='_blank'><AiOutlineGithub /> Source Code</a>
-              <a href='https://lucas-crodrigues.github.io/to-do-list/dist/' className='btn btn-primary' target='_blank'><AiOutlineGlobal /> Live Version</a>
+              <a href='https://github.com/lucas-crodrigues/to-do-list' className='btn' target='_blank' rel="noreferrer"><AiOutlineGithub /> Source Code</a>
+              <a href='https://lucas-crodrigues.github.io/to-do-list/dist/' className='btn btn-primary' target='_blank' rel="noreferrer"><AiOutlineGlobal /> Live Version</a>
             </div>
           </article>
         </div>
