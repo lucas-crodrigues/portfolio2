d531b36297a88979b96c09bff21feb2f71c69f31
lucascr <lcrodrigues1995@gmail.com>
2023-02-08 18:11:32 -0300

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './contact.css'
+import {AiOutlineMail, AiOutlineWhatsApp} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>Contact Me</h2>
+      <div className='container contact-container'>
+      <article className='contact-option'>
+          <h2><AiOutlineMail /></h2>
+          <h4>Email</h4>
+          <h5>cr.lucasrodrigues@gmail.com</h5>
+          <a href='mailto:cr.lucasrodrigues@gmail.com'>Send an Email</a>
+        </article>
+        <article className='contact-option'>
+          <h2><AiOutlineWhatsApp /></h2>
+          <h4>WhatsApp</h4>
+          <h5>Personal Phone Number</h5>
+          <a href='https://wa.me/+5547999276195' target='_blank' rel="noreferrer">Message Me</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
