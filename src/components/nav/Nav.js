2c9c97c2c90a530cdfd81ac4311175bab21950e0
lucascr <lcrodrigues1995@gmail.com>
2023-02-08 18:11:53 -0300

    Update Nav.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,18 +4,18 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineAlignLeft} from 'react-icons/ai'
 import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 import {AiOutlineFolderOpen} from 'react-icons/ai'
-import {AiOutlineTrophy} from 'react-icons/ai'
+// import {AiOutlineTrophy} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <nav className='nav-bar'>
-      <a href='#'><AiOutlineHome/> Home</a>
-      <a href='#about'><AiOutlineAlignLeft/> About</a>
-      <a href='#experience'><AiOutlineFundProjectionScreen/> Experience</a>
-      <a href='#portfolio'><AiOutlineFolderOpen/> Portfolio</a>
-      <a href='#testimonials'><AiOutlineTrophy/> Testimonials</a>
-      <a href='#contact'><AiOutlineMail/> Contact</a>
+      <a href='#'><AiOutlineHome/><p className='hide'>Home</p></a>
+      <a href='#about'><AiOutlineAlignLeft/><p className='hide'>About</p></a>
+      <a href='#experience'><AiOutlineFundProjectionScreen/><p className='hide'>Experience</p></a>
+      <a href='#portfolio'><AiOutlineFolderOpen/><p className='hide'>Portfolio</p></a>
+      {/* <a href='#testimonials'><AiOutlineTrophy/> Testimonials</a> */}
+      <a href='#contact'><AiOutlineMail/><p className='hide'>Contact</p></a>
     </nav>
   )
 }
